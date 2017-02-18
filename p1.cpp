f3e4599aa2cd786a49d1a51d7a7569ca54539f57
dgiebs <dgiebisch@gmail.com>
2017-02-18 15:35:50 -0500

    setup

--- p1.cpp
+++ p1.cpp
@@ -4,11 +4,18 @@
 
 use namespace std;
 
+float weight(vector<float> node1, vector<float> node2, int dimension);
+
 int main()
 {
-	
+
 }
 
-int weight(vector){
-
+float weight(vector<float> node1, vector<float> node2, int dimension){
+	
+	float differences = 0;
+	for (i = 0; i < dimension; i++){
+		differences += (node1[i] - node2[i])^2;
+	}
+	return sqrt(differences);
 }
