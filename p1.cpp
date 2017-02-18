3eb3a32ad2b4eb5df202d2d1cd247f7f4056c26b
dgiebs <dgiebisch@gmail.com>
2017-02-18 15:54:55 -0500

    init

--- p1.cpp
+++ p1.cpp
@@ -8,6 +8,19 @@ float weight(vector<float> node1, vector<float> node2, int dimension);
 
 int main()
 {
+	int dimention = 5;
+	int n = 10;
+
+	vector<float> nodes(dimension, 0)[n];
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < dimension; j++){
+        	nodes[i][j] = rand() % 1;
+        	printf("%f ", nodes[i][j]);
+    	}
+    	printf("\n");
+	}
+
 
 }
 
